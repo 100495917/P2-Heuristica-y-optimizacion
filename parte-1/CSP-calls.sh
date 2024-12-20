fa2088d52f0f7941533fc6a39655633df6dacc0b
100495772 <100495772@alumnos.uc3m.es>
2024-12-20 15:22:59 +0100

    permisos de ejecucion y python3 para csp-calls.sh

--- parte-1/CSP-calls.sh
+++ parte-1/CSP-calls.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-python CSPMaintenance.py ./CSP-tests/maintenance01
-python CSPMaintenance.py ./CSP-tests/maintenance02
-python CSPMaintenance.py ./CSP-tests/maintenance03
-python CSPMaintenance.py ./CSP-tests/maintenance04
-python CSPMaintenance.py ./CSP-tests/maintenance05
-python CSPMaintenance.py ./CSP-tests/maintenance06
-python CSPMaintenance.py ./CSP-tests/maintenance07
-python CSPMaintenance.py ./CSP-tests/maintenance08
-python CSPMaintenance.py ./CSP-tests/maintenance09
-python CSPMaintenance.py ./CSP-tests/maintenance10
-python CSPMaintenance.py ./CSP-tests/maintenance11
-python CSPMaintenance.py ./CSP-tests/maintenance12
-python CSPMaintenance.py ./CSP-tests/maintenance13
+python3 CSPMaintenance.py ./CSP-tests/maintenance01
+python3 CSPMaintenance.py ./CSP-tests/maintenance02
+python3 CSPMaintenance.py ./CSP-tests/maintenance03
+python3 CSPMaintenance.py ./CSP-tests/maintenance04
+python3 CSPMaintenance.py ./CSP-tests/maintenance05
+python3 CSPMaintenance.py ./CSP-tests/maintenance06
+python3 CSPMaintenance.py ./CSP-tests/maintenance07
+python3 CSPMaintenance.py ./CSP-tests/maintenance08
+python3 CSPMaintenance.py ./CSP-tests/maintenance09
+python3 CSPMaintenance.py ./CSP-tests/maintenance10
+python3 CSPMaintenance.py ./CSP-tests/maintenance11
+python3 CSPMaintenance.py ./CSP-tests/maintenance12
+python3 CSPMaintenance.py ./CSP-tests/maintenance13
